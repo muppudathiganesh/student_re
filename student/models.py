11bdb0786d1be46a8f8dca19630c6d1bad032e7e
muppudathiganesh <muppuraj11@gmail.com>
2025-08-02 19:44:58 +0530

    Add files via upload

---
+++ student/models.py
@@ -0,0 +1,12 @@
+# student/models.py
+
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    roll_no = models.CharField(max_length=20, unique=True)
+    grade = models.CharField(max_length=10)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.roll_no})"
